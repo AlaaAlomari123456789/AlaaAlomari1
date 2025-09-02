a61b5574ec4995c8a0da2197769da7e8be96a1ee
AlaaAlomari123456789 <alaaomari6789@gmail.com>
2025-09-02 16:12:07 +0300

    Add files via upload

---
+++ j.js
@@ -0,0 +1,6 @@
+// Example: Log message when page loads
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("Pancreas page loaded successfully!");
+});
+
+// You can add more JS functions here
